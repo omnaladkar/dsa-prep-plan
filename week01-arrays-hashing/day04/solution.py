a51479668d041f4041b05007ced3097a071a1afb
omnaladkar <75687080+omnaladkar@users.noreply.github.com>
2026-09-04 19:57:05 +0530

    Added Product of Array Except Self

--- week01-arrays-hashing/day04/solution.py
+++ week01-arrays-hashing/day04/solution.py
@@ -25,3 +25,54 @@ if __name__ == "__main__":
     # assert sol.methodName(input) == expected
     # print("All tests passed!")
 
+
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+       int prod = 1;
+       int zeros = 0;
+       
+    int n = nums.size();
+    vector<int> ans(n);
+    for(int i=0;i<n;i++){
+        if(nums[i] == 0){
+           zeros++;
+           continue;
+        }
+        
+        prod = prod*nums[i];
+
+
+    }
+
+    if(zeros > 0){
+        if(zeros > 0 && zeros > 1){
+            
+            for(int i=0;i<n;i++){
+            
+            ans[i] = 0;
+
+        }
+        return ans;
+
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+               ans[i] = prod; 
+               continue;
+            }
+            ans[i] = 0;
+
+        }
+    } else {
+      for(int i=0;i<n;i++){
+           
+            ans[i] = prod/nums[i];
+
+        }  
+    }
+
+    return ans;
+    }
+};
