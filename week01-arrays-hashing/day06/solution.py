94a752cf88350affbb4686c9c3fc5c680ea14bea
omnaladkar <om.naladkar@gmail.com>
2026-09-05 16:16:48 +0530

    Subarray sum of k

--- week01-arrays-hashing/day06/solution.py
+++ week01-arrays-hashing/day06/solution.py
@@ -25,3 +25,22 @@ if __name__ == "__main__":
     # assert sol.methodName(input) == expected
     # print("All tests passed!")
 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+       int n = nums.size();
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j=i;j<n;j++){
+               sum += nums[j];
+               if(sum == k){
+                count++;
+               } 
+            }
+        }
+
+        return count;
+    }
+};
