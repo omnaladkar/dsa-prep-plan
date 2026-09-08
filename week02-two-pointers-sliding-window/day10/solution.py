d878db5a844020b8afe7dc4c12c6c1303710976f
omnaladkar <om.naladkar@gmail.com>
2026-09-08 23:43:05 +0530

    two pointers, move smaller side

--- week02-two-pointers-sliding-window/day10/solution.py
+++ week02-two-pointers-sliding-window/day10/solution.py
@@ -1,27 +1,59 @@
-﻿"""
-Day 10 - Container With Most Water
-Link: https://leetcode.com/problems/container-with-most-water/
+﻿# """
+# Day 10 - Container With Most Water
+# Link: https://leetcode.com/problems/container-with-most-water/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        // int sum = 0;
+        // for(int i=0;i<height.size();i++){
+        //     int j = i+1;
+        //     while(j < height.size()){
+        //         int t = min(height[i], height[j]);
+        //         sum = max(sum, t*(j-i));
+        //         j++;
+        //     }
+        // }
+
+        // return sum;
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+    
+        int sum = 0;
+        while(left < right){
+            int mins = min(height[left], height[right]);
+            sum = max(sum, mins*(right - left));
+            if(height[left] > height[right]){
+                right--;
+            } else{
+                left++;
+            }
+        }
+
+        return sum;
+    }
+};
