4c9f15d7513df9df47c2adbf6951421a16008de7
omnaladkar <om.naladkar@gmail.com>
2026-09-14 22:58:54 +0530

    added code for linkedin like

--- week03-linked-lists-stacks-queues/day15/solution.py
+++ week03-linked-lists-stacks-queues/day15/solution.py
@@ -1,27 +1,57 @@
-﻿"""
-Day 15 - Reverse Linked List
-Link: https://leetcode.com/problems/reverse-linked-list/
+﻿# """
+# Day 15 - Reverse Linked List
+# Link: https://leetcode.com/problems/reverse-linked-list/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
+
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+       ListNode* prev = NULL;
+       ListNode* curr = head;
+
+
+       while(curr != NULL){
+
+        ListNode* next2 = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next2;
+
+       }
+
+       return prev;
+    }
+};
 
