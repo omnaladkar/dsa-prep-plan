7cfcc0afafd76614bea24fca5822868f934dea78
omnaladkar <om.naladkar@gmail.com>
2026-09-14 23:25:10 +0530

    finding the cycle in lst

--- week03-linked-lists-stacks-queues/day16/solution.py
+++ week03-linked-lists-stacks-queues/day16/solution.py
@@ -1,27 +1,53 @@
-﻿"""
-Day 16 - Linked List Cycle
-Link: https://leetcode.com/problems/linked-list-cycle/
+﻿# """
+# Day 16 - Linked List Cycle
+# Link: https://leetcode.com/problems/linked-list-cycle/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow  = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
