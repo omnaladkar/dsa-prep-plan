84046c3c01fdd69b2399a413a24d2e33dcb130e0
omnaladkar <om.naladkar@gmail.com>
2026-09-14 23:47:24 +0530

    merged two lists

--- week03-linked-lists-stacks-queues/day17/solution.py
+++ week03-linked-lists-stacks-queues/day17/solution.py
@@ -1,27 +1,63 @@
-﻿"""
-Day 17 - Merge Two Sorted Lists
-Link: https://leetcode.com/problems/merge-two-sorted-lists/
+﻿# """
+# Day 17 - Merge Two Sorted Lists
+# Link: https://leetcode.com/problems/merge-two-sorted-lists/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* secon = new ListNode(0);
+        ListNode* curr = secon;
+
+        while(list1 && list2){
+            if(list1->val > list2->val){
+                curr->next = list2;
+                list2 = list2->next;
+            } else  {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            curr = curr->next;
+        } 
+
+        curr->next = list1 ? list1 : list2;
+       ListNode* ans = secon->next;
+
+       delete secon;
+       return ans;
+
+
+    }
+};
