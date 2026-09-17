4df078739138cb129d18073c2a0f97560e6b60ed
omnaladkar <om.naladkar@gmail.com>
2026-09-17 23:38:36 +0530

    same tree

--- week04-trees-part1/day24/solution.py
+++ week04-trees-part1/day24/solution.py
@@ -1,27 +1,57 @@
-﻿"""
-Day 24 - Same Tree
-Link: https://leetcode.com/problems/same-tree/
+﻿# """
+# Day 24 - Same Tree
+# Link: https://leetcode.com/problems/same-tree/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL){
+            return true;
+        }
+        if((p == NULL && q != NULL) || (p != NULL && q == NULL)){
+            return false;
+        }
+
+        if(p->val != q->val){
+           
+            return false;
+        }
+
+        return  isSameTree(p->left, q->left) &&
+            isSameTree(p->right, q->right);
+    }
+};
