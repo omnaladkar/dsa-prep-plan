2624d9fcbf7db41c4bd3ecc5cc4a1de53b01bb0a
omnaladkar <om.naladkar@gmail.com>
2026-09-17 23:05:56 +0530

    First tree code recursive

--- week04-trees-part1/day22/solution.py
+++ week04-trees-part1/day22/solution.py
@@ -1,27 +1,49 @@
-﻿"""
-Day 22 - Maximum Depth of Binary Tree
-Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+﻿# """
+# Day 22 - Maximum Depth of Binary Tree
+# Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if(root == NULL){
+            return 0;
+        }
+
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
