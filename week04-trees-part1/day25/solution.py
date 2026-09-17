da90cfedad10b3644aeb381245e770404ea0a3a8
omnaladkar <om.naladkar@gmail.com>
2026-09-18 00:01:10 +0530

    BFS so for we need to use queue first add the root val then  then iterate accodin t queue size then we need to like that

--- week04-trees-part1/day25/solution.py
+++ week04-trees-part1/day25/solution.py
@@ -1,27 +1,79 @@
-﻿"""
-Day 25 - Binary Tree Level Order Traversal
-Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+﻿# """
+# Day 25 - Binary Tree Level Order Traversal
+# Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if(root == NULL){
+            return ans;
+        }
+
+        if(!root->left && !root->right){
+            vector<int> p;
+            p.push_back(root->val);
+            ans.push_back(p);
+            return ans;
+        }
+
+        queue<TreeNode*> q;
+
+        q.push(root);
+
+        while(q.size()){
+            vector<int> arr;
+            int s = q.size();
+
+            for(int i=0;i<s;i++){
+                TreeNode* p = q.front();
+                q.pop();
+                if(p->left) q.push(p->left);
+                if(p->right) q.push(p->right);
+                arr.push_back(p->val);
+            }
+
+            ans.push_back(arr);
+        }
+
+
+        return ans;
+
+
+    }
+};
