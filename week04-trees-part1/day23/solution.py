90f2aafd92979b3ad32a9e03f216972fec1cb5ff
omnaladkar <om.naladkar@gmail.com>
2026-09-17 23:17:02 +0530

    invert

--- week04-trees-part1/day23/solution.py
+++ week04-trees-part1/day23/solution.py
@@ -1,27 +1,43 @@
-﻿"""
-Day 23 - Invert Binary Tree
-Link: https://leetcode.com/problems/invert-binary-tree/
+﻿# """
+# Day 23 - Invert Binary Tree
+# Link: https://leetcode.com/problems/invert-binary-tree/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL){
+            return nullptr;
+        }
+
+        
+
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
