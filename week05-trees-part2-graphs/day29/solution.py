7e50c5d3c831cab6db85c1f5bcb37ff7ff2cb566
omnaladkar <om.naladkar@gmail.com>
2026-09-22 01:15:19 +0530

    Simple ne

--- week05-trees-part2-graphs/day29/solution.py
+++ week05-trees-part2-graphs/day29/solution.py
@@ -1,27 +1,58 @@
-﻿"""
-Day 29 - Kth Smallest Element in a BST
-Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+﻿# """
+# Day 29 - Kth Smallest Element in a BST
+# Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inOrder(TreeNode* r, vector<int>& ans){
+        if(!r){
+            return;
+        }
+        inOrder(r->left, ans);
+        ans.push_back(r->val);
+        inOrder(r->right, ans);
+
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        
+        vector<int> ans;
+         inOrder(root, ans);
+
+         return ans[k-1];
+    }
+};
