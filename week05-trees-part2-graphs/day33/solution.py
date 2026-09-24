e01a177d73d7f5064101c00df517c2c7a0578a01
omnaladkar <om.naladkar@gmail.com>
2026-09-25 01:55:11 +0530

    dine

--- week05-trees-part2-graphs/day33/solution.py
+++ week05-trees-part2-graphs/day33/solution.py
@@ -1,27 +1,75 @@
-﻿"""
-Day 33 - Course Schedule II
-Link: https://leetcode.com/problems/course-schedule-ii/
+﻿# """
+# Day 33 - Course Schedule II
+# Link: https://leetcode.com/problems/course-schedule-ii/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+class Solution {
+public:
+    vector<int> findOrder(int nC, vector<vector<int>>& pq) {
+        vector<vector<int>> graph(nC);
+        vector<int> inDegree(nC, 0);
+        vector<int> ans;
+
+        for(auto i : pq){
+            int b = i[1];
+            int a = i[0];
+
+            graph[b].push_back(a);
+            inDegree[a]++;
+        }
+
+        queue<int> q;
+
+        int count = 0;
+
+        for(int i = 0; i < nC; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int top = q.front();
+            q.pop();
+
+            ans.push_back(top);   // add when processed
+            count++;
+
+            for(int nex : graph[top]){
+                
+                inDegree[nex]--;
+
+                if(inDegree[nex] == 0){
+                    q.push(nex);
+                }
+            }
+        }
+
+        if(count == nC)
+            return ans;
+
+        return {};
+    }
+};
