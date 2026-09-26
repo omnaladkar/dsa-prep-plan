8780fd4f4ce84a500217205cb9380852b78b99ee
omnaladkar <om.naladkar@gmail.com>
2026-09-26 16:08:59 +0530

    dfs

--- week05-trees-part2-graphs/day34/solution.py
+++ week05-trees-part2-graphs/day34/solution.py
@@ -1,27 +1,57 @@
-﻿"""
-Day 34 - Number of Provinces
-Link: https://leetcode.com/problems/number-of-provinces/
+﻿# """
+# Day 34 - Number of Provinces
+# Link: https://leetcode.com/problems/number-of-provinces/
 
-Pattern Trigger:
-    [Write your pattern trigger here after solving]
+# Pattern Trigger:
+#     [Write your pattern trigger here after solving]
 
-Approach:
-    - [Write your approach here]
+# Approach:
+#     - [Write your approach here]
 
-Time Complexity:  O(?)
-Space Complexity: O(?)
-"""
-
-
-class Solution:
-    def methodName(self, params):
-        pass
+# Time Complexity:  O(?)
+# Space Complexity: O(?)
+# """
 
 
-# ---------- Test Cases ----------
-if __name__ == "__main__":
-    sol = Solution()
-    # Add test cases here
-    # assert sol.methodName(input) == expected
-    # print("All tests passed!")
+# class Solution:
+#     def methodName(self, params):
+#         pass
 
+
+# # ---------- Test Cases ----------
+# if __name__ == "__main__":
+#     sol = Solution()
+#     # Add test cases here
+#     # assert sol.methodName(input) == expected
+#     # print("All tests passed!")
+
+class Solution {
+public:
+
+    void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visited){
+        visited[node] = true;
+        int n = isConnected.size();
+
+        for(int i=0;i<n ; i++){
+            if(isConnected[node][i] == 1 && !visited[i]){
+                dfs(i, isConnected, visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+
+        int provinces = 0;
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i, isConnected, visited);
+                provinces++;
+            }
+
+        }
+
+        return provinces;
+    }
+};
